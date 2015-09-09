1c33dfd71a068a276d80fe4dd4f6c0e3ecef2afe
sammyrms1 <super.sammy2011@gmail.com>
2015-09-09 11:26:01 -0700

    Keep Rollin Those Dice

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,9 +1,25 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
+public void setup()
 {
 	noLoop();
 	size(640, 480);
 }
-void draw()
+public void draw()
 {
 	for (int x = 0; x < 640; x += 40) {
 		for (int y = 0; y < 480; y += 40) {
@@ -15,7 +31,7 @@ void draw()
 
 
 }
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -28,7 +44,7 @@ class Die //models one single dice cube
 		dieY = y;
 
 	}
-	void roll()
+	public void roll()
 	{
 		//Random Roll
 		rollNum = (int)(Math.random()*6)+1;
@@ -68,10 +84,19 @@ class Die //models one single dice cube
 			ellipse(dieX+30, dieY+30, 10, 10);
 		}
 	}
-	void show()
+	public void show()
 	{
 		fill(255);
 		rect(dieX, dieY, 40, 40);
 		fill(0);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
